02c2acea59be3088240e37d1b48cdf43e2623137
Fariborz <fariborz@localhost>
2026-02-12 00:39:03 +0330

    Restore missing settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,22 +7,16 @@ pluginManagement {
         }
         val flutterSdk = properties.getProperty("flutter.sdk")
         if (flutterSdk != null) return@run flutterSdk
-        
+
         val flutterRoot = System.getenv("FLUTTER_ROOT")
         if (flutterRoot != null) return@run flutterRoot
-        
-        error("Flutter SDK not found.")
+
+        error("flutter.sdk not set in local.properties nor FLUTTER_ROOT environment variable")
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        // اول علی‌بابا (چون سریع است)
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        
-        // اگر در علی‌بابا نبود، برو سراغ اصلی‌ها (با پروکسی)
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -31,8 +25,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
 include(":app")
